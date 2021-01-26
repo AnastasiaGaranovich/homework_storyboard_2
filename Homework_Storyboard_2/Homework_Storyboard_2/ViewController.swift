20f0737ad0f43632581a3f0a085018633a39df78
NastyaGaranovich <nastyamadness@gmail.com>
2021-01-26 16:01:07 +0300

    Create project, add three views and button

---
+++ Homework_Storyboard_2/Homework_Storyboard_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_Storyboard_2
+//
+//  Created by Анастасия Гаранович on 26.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
