cfc08561ea19981960819bd19ca3716fd77dd777
NastyaGaranovich <nastyamadness@gmail.com>
2021-01-27 13:19:55 +0300

    Add constraints, describe the logic of the traffic light, make outlets for button and views, make action for button

--- Homework_Storyboard_2/Homework_Storyboard_2/ViewController.swift
+++ Homework_Storyboard_2/Homework_Storyboard_2/ViewController.swift
@@ -1,18 +1,52 @@
-//
-//  ViewController.swift
-//  Homework_Storyboard_2
-//
-//  Created by Анастасия Гаранович on 26.01.21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var index = 1
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var startNextButton: UIButton!
+    
+    @IBAction func startNextButton(_ sender: UIButton) {
+        switch index {
+        case 1:
+            redColorView.backgroundColor = UIColor.red.withAlphaComponent(1)
+            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            startNextButton.setTitle("Next", for: .normal)
+        case 2:
+            redColorView.backgroundColor = UIColor.red.withAlphaComponent(1)
+            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            startNextButton.setTitle("Next", for: .normal)
+        case 3:
+            redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(1)
+            startNextButton.setTitle("Next", for: .normal)
+        case 4:
+            redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            startNextButton.setTitle("Next", for: .normal)
+        default:
+            break
+        }
+        index += 1
+        if index == 5 {
+            index = 1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+        greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+        startNextButton.setTitle("Start", for: .normal)
     }
+    
 
 
 }
